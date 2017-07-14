ed11005e08799f5bb1bb9cd31c2a08bc66d3d5e0
李晨鸣 <lichenming@link-pay.cn>
2017-07-14 17:42:36 +0800

    no message

---
+++ magicbutton/src/main/java/com/lcm/magicbutton/OnCheckChangeListenter.java
@@ -0,0 +1,14 @@
+package com.lcm.magicbutton;
+
+/**
+ * ****************************************************************
+ * Author: LCM
+ * Date: 2017/7/14 下午4:53
+ * Desc:
+ * *****************************************************************
+ */
+
+public interface OnCheckChangeListenter {
+
+    void onCheck(boolean isChecked);
+}
